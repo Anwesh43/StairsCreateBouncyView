9d8e19c2401cf277fd6eaa378a7426b71f445f1a
Anwesh43 <anweshthecool0@gmail.com>
2020-01-17 11:14:01 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedstepcreatebouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepcreatebouncyview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedstepcreatebouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.stepcreatebouncyview.StepCreateBouncyView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepCreateBouncyView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
