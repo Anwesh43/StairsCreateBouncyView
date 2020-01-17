be68b57e1d0c4fc2fff1c064b73e53901f3e30a4
Anwesh43 <anweshthecool0@gmail.com>
2020-01-17 10:50:07 +0530

    adding Int and Float extension method

--- stepcreatebouncyview/src/main/java/com/anwesh/uiprojects/stepcreatebouncyview/StepCreateBouncyView.kt
+++ stepcreatebouncyview/src/main/java/com/anwesh/uiprojects/stepcreatebouncyview/StepCreateBouncyView.kt
@@ -19,3 +19,8 @@ val scGap : Float = 0.02f / lines
 val delay : Long = 20L / lines
 val backColor : Int = Color.parseColor("#BDBDBD")
 val foreColor: Int = Color.parseColor("#3F51B5")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
