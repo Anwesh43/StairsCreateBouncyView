8ecb21fd7e18ff5c248795c03db8148ce79bc1c8
Anwesh43 <anweshthecool0@gmail.com>
2020-01-17 11:05:34 +0530

    adding SCBNode class

--- stepcreatebouncyview/src/main/java/com/anwesh/uiprojects/stepcreatebouncyview/StepCreateBouncyView.kt
+++ stepcreatebouncyview/src/main/java/com/anwesh/uiprojects/stepcreatebouncyview/StepCreateBouncyView.kt
@@ -119,4 +119,45 @@ class StepCreateBouncyView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SCBNode(var i : Int, val state : State = State()) {
+
+        private var next : SCBNode? = null
+        private var prev : SCBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SCBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SCBNode {
+            var curr : SCBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
