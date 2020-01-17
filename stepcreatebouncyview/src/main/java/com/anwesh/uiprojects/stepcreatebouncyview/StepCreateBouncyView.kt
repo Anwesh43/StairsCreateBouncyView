d73aa0c10f38f5ee98b376ad8cbee5103798f91c
Anwesh43 <anweshthecool0@gmail.com>
2020-01-17 10:58:15 +0530

    adding Canvas extension method

--- stepcreatebouncyview/src/main/java/com/anwesh/uiprojects/stepcreatebouncyview/StepCreateBouncyView.kt
+++ stepcreatebouncyview/src/main/java/com/anwesh/uiprojects/stepcreatebouncyview/StepCreateBouncyView.kt
@@ -24,3 +24,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStepLine(i : Int, scale : Float, size : Float, paint : Paint) {
+    val wsize : Float = ((size * 2)/ lines)
+    val ik : Int = i % 2
+    val jk : Int = i / 2
+    val sf : Float = scale.sinify().divideScale(i, lines)
+    save()
+    translate(jk * wsize + ik * size, jk * wsize)
+    drawLine(0f, 0f, wsize * (1 - jk) * sf, wsize * ik * sf, paint)
+    restore()
+}
+
+fun Canvas.drawStepLines(scale : Float, size : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawStepLine(j, scale, size, paint)
+    }
+}
+
+fun Canvas.drawSCBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * i, i * gap)
+    drawStepLines(scale, gap, paint)
+    restore()
+}
